bad8b86b4256535e579b9a0b855bd8a73b4af410
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2019-12-20 09:57:29 +0100

    Initial commit

---
+++ src/Card/Card.java
@@ -0,0 +1,8 @@
+package Card;
+
+public interface Card {
+    void playCard();
+    String getName();      //les méthodes d'une interface ne peuvent pas avoir de corps (contenu)
+    String getDescription();
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+package Card;
+public class Main {
+
+    public static void main(String[] args) {
+        Player player1 = new Player("PremierJoueur","Vert",'N',1);
+        player1.direction[0];
+
+    }
+}
